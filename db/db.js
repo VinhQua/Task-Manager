2f676223333c2670dea9f4f279fce0de7e3b2348
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-09-05 09:35:44 +0700

    connect to db

--- db/db.js
+++ db/db.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  password: "02032001",
-  host: "localhost",
-  port: 5432,
-  database: "taskmanager",
+  password: "jJabI4fAcg5xAkqpsD6j",
+  host: "containers-us-west-116.railway.app",
+  port: 7400,
+  database: "railway",
 });
 
 module.exports = pool;
