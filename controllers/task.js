0720f4c3c4a2d33e8d9e5f70997bcbfc195fad34
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-09-05 15:34:13 +0700

    Completed controller

--- controllers/task.js
+++ controllers/task.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const pool = require("../db/db");
 
 const getAllTask = async (req, res) => {
@@ -15,12 +16,12 @@ const getSingleTask = async (req, res) => {
     const task = await pool.query("SELECT * FROM task WHERE id = $1", [id]);
 
     if (task.rows.length === 0) {
-      return res.status(200).json({ msg: `success no task with id :${id}` });
+      return res.status(200).json({ msg: ` no task with id :${id}` });
     }
 
     return res.status(200).json({ msg: `success`, task: task.rows[0] });
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return res.status(404).json({ msg: `no task with id${id}` });
   }
 };
@@ -39,17 +40,35 @@ const createTask = async (req, res) => {
     console.log(error);
   }
 };
-const UpdateTask = (req, res) => {
+const UpdateTask = async (req, res) => {
   const { id } = req.params;
-  const { name } = req.body;
+  const { name, completed } = req.body;
 
   try {
-  } catch (error) {}
-  res.status(200).json({ msg: `update task with id ${id} and name ${name}` });
+    const task = await pool.query(
+      "UPDATE task SET name = $1, completed = $2 WHERE id = $3",
+      [name, completed, id]
+    );
+    if (task.rowCount === 0) {
+      return res.status(200).json({ msg: `no task with id ${id} to update` });
+    }
+    return res.status(200).json({ msg: `update`, task });
+  } catch (error) {
+    res.status(401).json({ msg: error });
+  }
 };
-const deleteTask = (req, res) => {
+const deleteTask = async (req, res) => {
   const { id } = req.params;
-  res.status(200).json({ msg: `delete task with id ${id}` });
+
+  try {
+    const task = await pool.query("DELETE FROM task WHERE id = $1", [id]);
+    if (task.rowCount === 0) {
+      return res.status(200).json({ msg: `no task with id ${id} to delete` });
+    }
+    return res.status(200).json({ msg: `deleted`, task });
+  } catch (error) {
+    res.status(401).json({ msg: error });
+  }
 };
 
 module.exports = {
